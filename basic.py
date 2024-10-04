a3211f19a4cf6d5378e70d08e59398277725fca1
Anis <imarahmandev@gmail.com>
2024-10-05 00:12:00 +0100

    :zap: derived class

--- basic.py
+++ basic.py
@@ -10,3 +10,22 @@ print(honda.make , honda.model, honda.year, honda.type)
 
 bike = Vehicle('Honda', 'CBR', 2019, 'motorcycle')
 print(bike.make , bike.model, bike.year, bike.type)
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, type, doors):
+        super().__init__(make, model, year, type)
+        self.doors = doors
+
+civic = Car('Honda', 'Civic', 2019, 'car', 4)
+
+print(civic.make , civic.model, civic.year, civic.type, civic.doors)
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, make, model, year, type, engine):
+        super().__init__(make, model, year, type)
+        self.engine = engine
+
+cbr = Motorcycle('Honda', 'CBR', 2019, 'motorcycle', '1000cc')
+
+print(cbr.make , cbr.model, cbr.year, cbr.type, cbr.engine)
